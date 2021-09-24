f9ee5e840221aa4da91df7d2a4ae764c7251524a
aniram007 <70104287+aniram007@users.noreply.github.com>
2021-09-25 02:39:26 +0530

    Add files via upload

--- 14)FACE_DATA_PREP.py
+++ 14)FACE_DATA_PREP.py
@@ -4,15 +4,15 @@ import cv2 												#Image processing library.
 import os 													#Library for file path handling.
 import random 												#To work with random operations.
 ##########################################DATASET PREPARATION##########################################
-directory = "Corona Mask"											#Destination directory which contains images.
-emotions = ["Mask","No Mask"] 										#These folders will be accessed by the program for data.
+directory = "Face_Emotions"											#Destination directory which contains images.
+emotions = ["Angry","Happy","Normal","Sad","Shocked"] 								#These folders will be accessed by the program for data.
 training_data = [] 												#Empty array to hold the emotion data.
 for emotion in emotions: 											#First loop to select the folder.
 	path = directory+"/"+emotion 										#Append the destination directory with the folder name.
 	index = emotions.index(emotion) 										#Get the index value of the current emotion. This is the target value.
 	for img in os.listdir(path): 										#Second loop to select the image.
 		try:											#Some images might not load properly.
-			img_array = cv2.imread(path+"/"+img)							#Load the image into the program as a numpy array.
+			img_array = cv2.imread(path+"/"+img,0)							#Load the image into the program as a numpy array.
 			new_array = cv2.resize(img_array, (50,50)) 						#Resize the image to reduce computational complexity.
 			training_data.append([new_array,index]) 							#Attach the current input and target to the dataset.
 		except: 											#When an error occurs.
@@ -21,5 +21,5 @@ random.shuffle(training_data) 											#Shuffle the dataset to prevent overfit
 data = training_data[:10]											#Get the first 10 images and labels.
 for features,labels in data: 											#Loop through the sample
 	print(labels) 											#Dsiplay the shuffled label values.
-np.save("Datasets/MASK_ANI.npy",training_data) 								#Save the training data in the Datasets folder.
+np.save("Datasets/FACE_TRAIN.npy",training_data) 								#Save the training data in the Datasets folder.
 #########################################################################################################
